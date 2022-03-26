4bd4622553de847f1f4e2072d3ec890ed2f4e4b9
CarlyFSS <carly.fs.silva@gmail.com>
2022-03-26 10:32:41 -0300

    chore: added TODO in HttpController and vscode settings
    Added TODO in HttpController substitute body type of unknow with a new type HttpParams

--- src/core/shared/presentation/contracts/http/HttpController.ts
+++ src/core/shared/presentation/contracts/http/HttpController.ts
@@ -1,5 +1,7 @@
 import { HttpResponse } from './HttpResponse';
 
+// TODO Create HttpParams to substitute the body parameter
+
 export interface HttpController {
   create(body: unknown): Promise<HttpResponse>;
 }
